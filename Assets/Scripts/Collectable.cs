21c016fbe01fbdc2a7f363eac26fa5f4fc286c80
loxer <loxer@live.de>
2020-05-10 14:22:49 +0200

    Just trying a few things, like a new play field

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -18,16 +18,16 @@ public class Collectable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3[] spawnPositions = new Vector3[numberOfCollectables];
+        // Vector3[] spawnPositions = new Vector3[numberOfCollectables];
 
-        for(int i = 0; i < numberOfCollectables; i++) 
-        {
+        // for(int i = 0; i < numberOfCollectables; i++) 
+        // {
 
-            Vector3 spawnPos = getSpawnPoint(spawnPositions);
-            spawnPositions[i] = spawnPos;
+        //     Vector3 spawnPos = getSpawnPoint(spawnPositions);
+        //     spawnPositions[i] = spawnPos;
             
-            Instantiate(collectable, spawnPos, Quaternion.identity);
-        }
+        //     Instantiate(collectable, spawnPos, Quaternion.identity);
+        // }
     
         // printPos(spawnPositions);
     }
