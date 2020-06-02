1927bb1acf0f527894b034da8f48d32560b209b2
loxer <loxer@live.de>
2020-06-02 14:36:57 +0200

    Getting started

---
+++ Assets/Scripts/Abgabe4/PlayerComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+
+public class PlayerComponent : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] float rotationAngle;
+    
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     EntityManager manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    //     manager.CreateEntity();
+    // }
+
+}
