fe4f495349627cb627d886813f0be4bdf761c616
loxer <loxer@live.de>
2020-06-02 15:54:53 +0200

    PlayerBehavior finished

---
+++ Assets/Scripts/Abgabe4/PlayerBehaviorSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using System;
+
+
+public class PlayerBehaviorSystem : SystemBase
+{    
+    protected override void OnUpdate() 
+    {
+        Entities.ForEach((ref PlayerComponent player, ref Translation translation, ref Rotation rotation) =>
+        {
+            player.rotationAngle = player.rotationAngle + Input.GetAxis("Horizontal");
+            float3 targetDirection = new float3((float) math.sin(player.rotationAngle), 0, (float) math.cos(player.rotationAngle));
+            
+            rotation.Value = quaternion.LookRotationSafe(targetDirection, Vector3.up);
+            translation.Value += player.speed * Input.GetAxis("Vertical") /* * new float3(1,0,1) */ * targetDirection;
+        }).WithoutBurst().Run();
+    }
+}
